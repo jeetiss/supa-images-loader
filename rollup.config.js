e0b90133cf61ea491129cf90baa0762d4459309d
Dmitry <za5rubley@gmail.com>
2018-03-07 10:00:29 +0300

    add umd build

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,43 @@
 import babel from 'rollup-plugin-babel'
+import uglify from 'rollup-plugin-uglify'
 import pkg from './package.json'
 
-export default {
+const umdBuild = minify => ({
   input: 'src/index.js',
   output: [
     {
-      file: pkg.main,
-      format: 'cjs',
-    },
-    {
-      file: pkg.module,
-      format: 'es',
+      name: 'supaImagesLoader',
+      file: minify ? 'dist/umd/index.min.js' : 'dist/umd/index.js',
+      format: 'umd',
     },
   ],
   plugins: [
     babel({
       exclude: 'node_modules/**',
     }),
+    minify ? uglify() : {},
   ],
-}
+})
+
+export default [
+  {
+    input: 'src/index.js',
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs',
+      },
+      {
+        file: pkg.module,
+        format: 'es',
+      },
+    ],
+    plugins: [
+      babel({
+        exclude: 'node_modules/**',
+      }),
+    ],
+  },
+  umdBuild(true),
+  umdBuild(false),
+]
